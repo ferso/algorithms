03dad528e99dfa3b4574932b97e00daf7e2fb70a
Fernando Soto <erickfernando@gmail.com>
2021-09-30 02:05:59 -0500

    Update getHandShakes.ts

--- combinations/getHandShakes.ts
+++ combinations/getHandShakes.ts
@@ -1,5 +1,5 @@
 ///get the number of the handshakes between a group of persons.
-
+/// math formula is  n*(n - 1)/2.
 const getHandShakes = (persons: number): number => {
   return (persons * (persons - 1)) / 2;
 };
