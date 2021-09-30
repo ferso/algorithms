c91017e8fc3f9acee8372c861a90b0b37f947bd7
Fernando Soto <erickfernando@gmail.com>
2021-09-30 02:08:24 -0500

    Update numberFactorGenerator.ts

--- combinations/numberFactorGenerator.ts
+++ combinations/numberFactorGenerator.ts
@@ -1,4 +1,5 @@
 /// Get factors numbers of a given number
+/// this a generator functions for es6 and typescript
 function* numberFactorGenerator(number: number): Generator<number> {
   let i: number = 0;
   while (i <= number) {
